cf5abe67d48b2f720cbeca54f4e9aaaaa1f6c86d
Rattle-Brain <danialgo2001@gmail.com>
2024-05-16 13:21:24 +0200

    go mod add

--- sra-monitor_ebpf/cmd/monitor.go
+++ sra-monitor_ebpf/cmd/monitor.go
@@ -1,1 +1,19 @@
 package main
+
+import "golang.org/x/sys/unix"
+
+type DataEnter struct {
+	PID       uint32
+	UID       uint32
+	Comm      [16]byte
+	Filename  [unix.NAME_MAX]byte
+	Timestamp uint64
+}
+
+type DataExit struct {
+	PID       uint32
+	UID       uint32
+	Comm      [16]byte
+	Timestamp uint64
+	Retval    int32
+}
