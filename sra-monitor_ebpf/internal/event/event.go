551ad4ba51dd54a8eff1ce8798cc9fe7bf7fc886
Rattle-Brain <danialgo2001@gmail.com>
2024-05-21 09:53:04 +0200

    Done test unmarshall

--- sra-monitor_ebpf/internal/event/event.go
+++ sra-monitor_ebpf/internal/event/event.go
@@ -3,6 +3,7 @@ package event
 import (
 	"encoding/binary"
 	"fmt"
+	"os/user"
 	"time"
 )
 
@@ -17,18 +18,31 @@ type Event struct {
 }
 
 func UnmarshallEntryEvent(marshd []byte) Event {
+	//First we need to parse the events. Since most of the time the
+	// Automatic process wil not do it properly with the offsets
+	// We need to do it manually like so.
 
+	// Initial byte for opcode to string
 	syscall_code := marshd[0]
 	syscall_name := getSyscallFromCode(syscall_code)
+
+	// Get PID and UID
 	pid := binary.LittleEndian.Uint32(marshd[4:8])
 	uid := binary.LittleEndian.Uint32(marshd[8:12])
+
+	// Find the username
+	username := getUsernameFromUid(uid)
+
+	// GEt Commname(LEN = 16) and Filename (LEN = 64)
 	comm := string(marshd[12:28])
 	filename := string(marshd[28:92])
+
+	// Timestamp (last bytes)
 	timestamp := binary.BigEndian.Uint64(marshd[92:108])
 	ts := time.Unix(0, int64(timestamp))
 
-	fmt.Printf("Syscall: %s, PID: %d, UID: %d, COMM: %s, FILENAME: %s, TIME: %s\n", syscall_name,
-		pid, uid, comm, filename, ts.Format(time.RFC3339Nano))
+	fmt.Printf("Syscall: %s, PID: %d, UID: %d, USER: %s, COMM: %s, FILENAME: %s, TIME: %s\n", syscall_name,
+		pid, uid, username, comm, filename, ts.Format(time.RFC3339Nano))
 
 	return Event{}
 }
@@ -41,12 +55,20 @@ func UnmarshallExit(marshd []byte) Event {
 func getSyscallFromCode(b byte) string {
 	switch b {
 	case 'o':
-		return "Syscall_Openat"
+		return "Syscall Openat"
 	case 'r':
-		return "Syscall_Read"
+		return "Syscall Read"
 	case 'w':
-		return "Syscall_Write"
+		return "Syscall sWrite"
 	default:
 		return "None"
 	}
+}
+
+func getUsernameFromUid(uid uint32) string {
+	u, err := user.LookupId(fmt.Sprint(uid))
+	if err != nil {
+		return "unknown"
+	}
+	return u.Username
 }
